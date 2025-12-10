081cc043feff894202894e056cb7f018287f2a04
phantekzy <mainilotfi@gmail.com>
2025-12-10 17:02:20 +0100

    added tutor session page

--- app/tutors/[id]/page.tsx
+++ app/tutors/[id]/page.tsx
@@ -1,7 +1,54 @@
+/* Import section */
+import { getTutor } from "@/lib/actions/tutor.actions"
+import { getSubjectColor } from "@/lib/utils"
+import { currentUser } from "@clerk/nextjs/server"
+import Image from "next/image"
+import { redirect } from "next/navigation"
+/* Interface section */
+interface TutorSessionPageProps {
+    params: Promise<{ id: string }>
+}
 /* Session section */
-const TutorSession = () => {
+const TutorSession = async ({ params }: TutorSessionPageProps) => {
+    const { id } = await params
+    const tutor = await getTutor(id)
+    const user = await currentUser()
+    if (!user) redirect('sign-in')
+    if (!tutor) redirect('/tutors')
     return (
-        <div>Session</div>
+        <main>
+            <article className="flex rounded-border justify-between p-6 max-md:flex-col">
+                <div className="flex items-center gap-2">
+
+                    <div className="size-[72px] flex items-center justify-center rounded-lg max-md:hidden"
+                        style={{ backgroundColor: getSubjectColor(tutor.subject) }}>
+                        <Image
+                            src={`/icons/${tutor.subject}.svg`}
+                            alt="subject icon"
+                            width={35}
+                            height={35}
+                        />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <div className="flex items-center gap-4">
+                            <p className="font-bold text-2xl">
+                                {tutor.name}
+                            </p>
+                            <div className="subject-badge max-sm:hidden">
+                                {tutor.subject}
+                            </div>
+                        </div>
+                        <p className="text-lg">
+                            {tutor.topic}
+                        </p>
+                    </div>
+                </div>
+                <div className=" flex items-center text-2xl max-md:hidden">
+                    {tutor.duration} minutes
+                </div>
+            </article>
+        </main>
     )
 }
 /* Export section */
--- lib/actions/tutor.actions.ts
+++ lib/actions/tutor.actions.ts
@@ -7,18 +7,15 @@ import { createSupabaseClient } from "../supabase";
 export const createTutor = async (formData: CreateTutor) => {
   const { userId: author } = await auth();
   const supabase = createSupabaseClient();
-
   const { data, error } = await supabase
     .from("Tutors")
     .insert({ ...formData, author })
     .select();
-
   if (error || !data)
     throw new Error(error?.message || "Failed to create your Tutor");
-
   return data[0];
 };
-
+/* Getting all the tutors */
 export const getAllTutors = async ({
   limit = 10,
   page = 1,
@@ -36,12 +33,15 @@ export const getAllTutors = async ({
   } else if (topic) {
     query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
   }
-
   query = query.range((page - 1) * limit, page * limit - 1);
-
   const { data: tutors, error } = await query;
-
   if (error) throw new Error(error.message);
-
   return tutors;
+};
+/* Getting a tutor */
+export const getTutor = async (id: string) => {
+  const supabase = createSupabaseClient();
+  const { data, error } = await supabase.from("Tutors").select().eq("id", id);
+  if (error) return console.log(error);
+  return data[0];
 };

c4a99829111408d4e2bfefd5a52ab7f8372dc368
phantekzy <mainilotfi@gmail.com>
2025-12-10 03:04:44 +0100

    added a functional filter button to filter base of subjects

--- components/SubjectFilter.tsx
+++ components/SubjectFilter.tsx
@@ -1,5 +1,47 @@
+'use client'
+/* Import section */
+import { useEffect, useState } from "react"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
+import { usePathname, useSearchParams, useRouter } from "next/navigation"
+import { subjects } from "@/constants"
+/* Filter subjects button */
 const SubjectFilter = () => {
-    return <div>Subject filter</div>
+    const pathname = usePathname()
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const [subject, setSubject] = useState(searchParams.get('subject') || 'All')
+    useEffect(() => {
+        const delay = setTimeout(() => {
+            const params = new URLSearchParams(searchParams.toString())
+            if (subject === 'All') {
+                params.delete('subject')
+            } else {
+                params.set('subject', subject)
+            }
+            router.push(`${pathname}?${params.toString()}`)
+        }, 300)
+
+        return () => clearTimeout(delay)
+    }, [subject])
+    return (
+        <Select value={subject} onValueChange={setSubject}>
+            <SelectTrigger className="input">
+                <SelectValue placeholder="Filters" />
+            </SelectTrigger>
+            <SelectContent>
+                <SelectItem value='All'>All</SelectItem>
+                {subjects.map((sub) => (
+                    <SelectItem
+                        key={sub}
+                        value={sub}
+                    >
+                        {sub}
+                    </SelectItem>
+                ))
+                }
+            </SelectContent>
+        </Select>
+    )
 }
 /* Export section */
 export default SubjectFilter

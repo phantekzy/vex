aff12e2c8eb994214428dc7af18b587e09ec9299
phantekzy <mainilotfi@gmail.com>
2025-12-13 15:11:05 +0100

    fixed the height

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 /* Sign-in section */
 export default function Page() {
     return (
-        <main className="flex items-center justify-center">
+        <main className="flex items-center justify-center min-h-[500px]">
             <SignIn />
         </main>
     )

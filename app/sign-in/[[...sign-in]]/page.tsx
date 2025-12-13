f36a3037f43d9c60eede644df1e1b8c9a0dce019
phantekzy <mainilotfi@gmail.com>
2025-12-13 18:44:52 +0100

    changed the colors for Clerck menus

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,18 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
     return (
         <main className="flex items-center justify-center min-h-[500px]">
-            <SignIn />
+            <SignIn
+                appearance={{
+                    variables: {
+                        colorPrimary: '#D62F2F',
+                    },
+                    elements: {
+                        card: {
+                            boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1)',
+                        }
+                    }
+                }}
+            />
         </main>
     )
 }
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -4,7 +4,19 @@ import { PricingTable } from "@clerk/nextjs"
 const Subscription = () => {
     return (
         <main className="py-12 px-4 max-w-5xl mx-auto">
-            <PricingTable />
+            <PricingTable
+                appearance={{
+                    variables: {
+                        colorPrimary: '#D62F2F',
+                    },
+                    elements: {
+                        card: {
+                            boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1)',
+                        }
+                    }
+                }}
+
+            />
         </main>
     )
 }
